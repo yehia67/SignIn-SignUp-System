26cacd58b8e1bdd206efc60d17613d1ea45d0e70
yehia67 <yehiatarek67@gmail.com>
2018-06-25 23:07:11 +0200

    first commit

---
+++ index.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html5>
+<html>
+  <head>
+    <meta charset="UTF-8">
+    <title>Login Page</title>
+    <meta name="description" content="A simple registartion System">
+    <link rel="stylesheet" href="css/style.css">
+  
+    <body>
+        <!-- Start Website-->
+        <div class="web">
+            <!-- Start login form-->
+            <form class ="js-login">
+              Username<br />
+              <input type ="text" require >
+              <br />
+              Password<br />
+              <input type ="password" require >
+              <br/>
+              <input type ="button" value ="login">
+              <br/>
+              <span class ="registrate">
+                  Don't have an account?<a href="signup.php">signup</a>
+              </span>
+            </form>
+             <!-- End login form-->
+         <!-- End Website-->
+        </div>
+    </body>
+</html>
