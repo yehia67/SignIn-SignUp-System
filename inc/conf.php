483993c0d5c2e89522e792b37bc4e3cd20f13d25
yehia67 <yehiatarek67@gmail.com>
2018-06-26 21:54:57 +0200

    create database

--- inc/conf.php
+++ inc/conf.php
@@ -4,6 +4,8 @@
         exit("Sorry you don't have accesse to this page");
     }
     //Start Configuration
-    
+    //include database
+    include_once "classes/DB.php";
+    $con = DB::getConnection();
     //End Configuration
 ?>
--- index.php
+++ index.php
@@ -3,7 +3,6 @@
     define('__CONFIG__',true);
     //Require Permisiion
     require_once "inc/conf.php";
-    
     ?>
 <!DOCTYPE html5>
 <html>
@@ -16,17 +15,17 @@
         <div class="web">
             <!-- Start login form-->
             <form class ="js-login">
-              Username<br />
-              <input type ="text" require >
-              <br />
-              Password<br />
-              <input type ="password" require >
-              <br/>
-              <input type ="button" value ="login">
-              <br/>
-              <span class ="registrate">
-                  Don't have an account?<a href="signup.php">signup</a>
-              </span>
+                email<br />
+                <input type ="email" require >
+                <br />
+                Password<br />
+                <input type ="password" require >
+                <br/>
+                <input type ="button" value ="login">
+                <br/>
+                <span class ="registrate">
+                Don't have an account?<a href="signup.php">signup</a>
+                </span>
             </form>
              <!-- End login form-->
          <!-- End Website-->
